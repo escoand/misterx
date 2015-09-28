fba2ae059184bde77c1feada58a527dd4787942f
escoand <escoand@users.noreply.github.com>
2015-09-28 14:39:42 +0200

    add styling

--- map/styles.js
+++ map/styles.js
@@ -1,4 +1,4 @@
-{
+var styles = {
 	"MrX 01": { "marker-color": "#f86767", "marker-symbol": "1" },
 	"MrX 02": { "marker-color": "#63b6e5", "marker-symbol": "2" },
 	"MrX 03": { "marker-color": "#a3e46b", "marker-symbol": "3" },
@@ -11,4 +11,4 @@
 	"MrX 10": { "marker-color": "#7ec9b1", "marker-symbol": "A" },
 	"MrX 11": { "marker-color": "#1087bf", "marker-symbol": "B" },
 	"MrX 12": { "marker-color": "#eaf7ca", "marker-symbol": "C" }
-}
+};
